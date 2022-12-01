bc73fd6b14aebf8269fbfd5749990866dfbed680
Nick Lamb <git@tree.tlrmx.org>
2022-12-01 17:41:06 +0000

    Use select_nth_unstable_by

--- src/day01.rs
+++ src/day01.rs
@@ -1,10 +1,10 @@
 use jungle::readfile;
+use jungle::Contents;
 
-pub fn a() {
-    let ctxt = readfile("01");
+fn calories(input: Contents) -> Vec<u64> {
     let mut elves = Vec::new();
     let mut total: u64 = 0;
-    for calorie in ctxt.lines() {
+    for calorie in input.lines() {
         if let Ok(c) = u64::from_str_radix(calorie, 10) {
             total += c;
         } else {
@@ -12,24 +12,20 @@ pub fn a() {
             total = 0;
         }
     }
-    elves.sort();
-    println!("Elf with most calories has: {}", elves.last().unwrap());
+    elves
+}
+
+pub fn a() {
+    let ctxt = readfile("01");
+    let mut elves = calories(ctxt);
+    let (_, most, _) = elves.select_nth_unstable_by(0, |a, b| b.cmp(a));
+    println!("Elf with most calories has: {most}");
 }
 
 pub fn b() {
     let ctxt = readfile("01");
-    let mut elves = Vec::new();
-    let mut total: u64 = 0;
-    for calorie in ctxt.lines() {
-        if let Ok(c) = u64::from_str_radix(calorie, 10) {
-            total += c;
-        } else {
-            elves.push(total);
-            total = 0;
-        }
-    }
-    elves.sort();
-    elves.reverse();
+    let mut elves = calories(ctxt);
+    elves.select_nth_unstable_by(2, |a, b| b.cmp(a));
     let top = &elves[0..3];
     let total: u64 = top.iter().sum();
     println!("Top three elves have {total}");
